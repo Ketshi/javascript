535a0c861d2bfa71a43b0feba7a1f2e29299c17b
Ketshi <kbavishi@id.iit.edu>
2017-03-31 11:40:12 -0500

    Created a click event Listener for the first box

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ boxes[0].style.backgroundColor = "blue"
 
 boxes[2].style.backgroundColor = "yellow"
 
+
+
+function boxClicked(){
+    console.log("Hey, box clicked!");
+}
+
+
+boxes[0].addEventListener("click", boxClicked);
 console.log(boxes[0]);

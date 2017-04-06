7d11330116b471f597b6ebf4350ea757d26a6cac
Ketshi <kbavishi@id.iit.edu>
2017-04-06 16:03:35 -0500

    Changed the color and added the click function for the 1st box

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ boxes[0].style.backgroundColor = "green"
 
 boxes[1].style.backgroundColor = "purple"
 
-boxes[2].style.backgroundColor = "purple"
+boxes[2].style.backgroundColor = " #191919"
 
-boxes[3].style.backgroundColor = "purple"
+boxes[3].style.backgroundColor = "blanchedalmond"
 
-boxes[4].style.backgroundColor = "purple"
+boxes[4].style.backgroundColor = "default"
 
 
 var leftMargin = 0;
@@ -32,3 +32,22 @@ function boxClicked() {
 boxes[0].addEventListener("click", boxClicked);
 console.log(boxes[0]);
 
+
+var moveAmount = 10;
+function boxClicked() {
+    console.log("Hey, box clicked!");
+
+
+
+    if (leftMargin < 100) {
+        leftMargin += moveAmount;
+
+    }
+    leftMargin += moveAmount;
+
+
+    boxes[1].style.marginLeft = leftMargin + "px";
+
+}
+boxes[1].addEventListener("clickOne", boxClicked);
+console.log(boxes[1]);

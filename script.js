08b23622b3f8d770b10bd38f76e7cf07677d5bea
Ketshi <kbavishi@id.iit.edu>
2017-04-06 15:09:59 -0500

    Added 5 boxes .

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 var boxes = document.querySelectorAll(".box");
 
-boxes[0].style.backgroundColor = "blue"
+boxes[0].style.backgroundColor = "green"
 
-boxes[2].style.backgroundColor = "yellow"
+boxes[1].style.backgroundColor = "purple"
+
+boxes[2].style.backgroundColor = "purple"
+
+boxes[3].style.backgroundColor = "purple"
+
+boxes[4].style.backgroundColor = "purple"
 
 
+var leftMargin = 0;
 
-function boxClicked(){
+var moveAmount = 5;
+function boxClicked() {
     console.log("Hey, box clicked!");
+
+
+
+    if (leftMargin < 100) {
+        leftMargin += moveAmount;
+
+    }
+    leftMargin += moveAmount;
+
+
+    boxes[0].style.marginLeft = leftMargin + "px";
+
 }
-
-
 boxes[0].addEventListener("click", boxClicked);
 console.log(boxes[0]);
+

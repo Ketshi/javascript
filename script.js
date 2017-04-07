7b4adf51b45966ad78c789dd770f9618ddda0b8d
Ketshi <kbavishi@id.iit.edu>
2017-04-07 10:45:00 -0500

    added function for 4th and 5th boxes

--- script.js
+++ script.js
@@ -14,10 +14,14 @@ boxes[4].style.backgroundColor = "default"
 var leftMargin = 0;
 var leftMargin1 = 0;
 var leftMargin2 = 0;
+var leftMargin3 = 0;
+var leftMargin4 = 0;
 
 var moveAmount = 5;
 var moveAmount1 = 10;
 var moveAmount2= 15;
+var moveAmount3 = 20;
+var moveAmount4= 25;
 
 function boxClicked() {
     console.log("Hey, box clicked!");
@@ -37,7 +41,21 @@ function box3Clicked() {
     boxes[2].style.marginLeft = leftMargin2 + "px";
 }
 
+function box4Clicked() {
+    console.log("Hey, box clicked!");
+    leftMargin3 += moveAmount3;
+    boxes[3].style.marginLeft = leftMargin3 + "px";
+}
+
+function box5Clicked() {
+    console.log("Hey, box clicked!");
+    leftMargin4 += moveAmount4;
+    boxes[4].style.marginLeft = leftMargin4 + "px";
+}
+
+
 boxes[0].addEventListener("click", boxClicked);
 boxes[1].addEventListener("click", box2Clicked);
 boxes[2].addEventListener("click", box3Clicked);
-
+boxes[3].addEventListener("dblclick", box4Clicked);
+boxes[4].addEventListener("click", box5Clicked);
